a1e0fd305569738d58a25933b0a5ce2c6f4a370b
GorYeritsyan <gor.k.yeritsyan@gmail.com>
2026-06-09 20:19:13 +0400

    Add search input inside chat sidebar

--- src/components/shared/messages/MessagesSidebar.tsx
+++ src/components/shared/messages/MessagesSidebar.tsx
@@ -1,6 +1,7 @@
 import {getFriends} from "@/actions/users";
 import {TUser} from "@/types/types";
 import Link from "next/link";
+import {Input} from "@/components/shadcn/input";
 
 export default async function MessagesSidebar() {
     const friends = await getFriends();
@@ -9,14 +10,19 @@ export default async function MessagesSidebar() {
     return (
         <div className="min-w-100 overflow-y-auto rounded-l-xl border-r border-zinc-200">
             <div>
-                {friends.map((friend: TUser) => (
-                    <Link href={`/messages/${friend.id}`} key={friend.id}>
-                        <div className="px-4 pl-6 py-3 border-b border-zinc-200 hover:bg-zinc-100 cursor-pointer flex flex-col">
-                            <h4 className="text-lg font-medium">{friend.name}</h4>
-                            <p className="text-zinc-500">{friend.email}</p>
-                        </div>
-                    </Link>
-                ))}
+                <div className="p-2">
+                    <Input />
+                </div>
+                <div>
+                    {friends.map((friend: TUser) => (
+                        <Link href={`/messages/${friend.id}`} key={friend.id}>
+                            <div className="px-4 pl-6 py-3 border-b border-zinc-200 hover:bg-zinc-100 cursor-pointer flex flex-col">
+                                <h4 className="text-lg font-medium">{friend.name}</h4>
+                                <p className="text-zinc-500">{friend.email}</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     )
